4cedf0e9bc62925aec0173cbed13a06fcf914831
Dennis Co <TheDennisaur@gmail.com>
2017-09-09 17:18:15 -0700

    Implement updating completed gestures

---
+++ Assets/Scripts/GestureObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GestureObject : MonoBehaviour {
+//	public Dictionary<string, Sprite> spriteList = new Dictionary<string, Sprite>();
+	public Sprite[] spriteList;
+	public string name;
+
+	// Use this for initialization
+	void Start () {
+		Image image = gameObject.GetComponent<Image> ();
+		image.sprite = GetRandomGesture ();
+		name = image.sprite.name;
+	}
+	
+	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	private Sprite GetRandomGesture() {
+		return spriteList[Random.Range (0, spriteList.Length)];
+	}
+}
